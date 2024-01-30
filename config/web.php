cd4a9d2c0e05e67a889a6da5d7165e57447a0f76
Lu Stormstout <lustormstout@gmail.com>
2024-01-30 11:03:57 +0800

    Implemented local database configuration in Yii2 Basic project similar to Yii2 Advanced

--- config/web.php
+++ config/web.php
@@ -3,6 +3,11 @@
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
+// Load local config if exists
+if (file_exists(__DIR__ . '/db-local.php')) {
+    $db = array_merge($db, require __DIR__ . '/db-local.php');
+}
+
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
