7886a5afea947d6d07e8464646adf479210b0f26
Lu Stormstout <lustormstout@gmail.com>
2024-02-01 16:42:30 +0800

    Define pageSize in config/params.php

--- config/params.php
+++ config/params.php
@@ -4,4 +4,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+
+    // 每页显示的条数
+    'pageSize' => 10,
 ];
